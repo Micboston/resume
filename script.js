b862646bd46b4f6f701c38b6488a9d23a21900d6
Micboston <61673031+Micboston@users.noreply.github.com>
2021-03-10 18:35:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+function valentine() {
+ location.replace("https://ecmbqa.mimo.run/index.html")
+}
+
+function micBoston() {
+ location.replace("https://micboston.github.io/index.html")
+}
+
+
+
+
+
+
+
